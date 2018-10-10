37ac63ce5b2171b33db85284f0d44d9d00e0ff61
sunthx <sunth0101@126.com>
2018-10-10 23:21:21 +0800

    修改默认的端口号：8090 && Dockfile

--- src/server/main.go
+++ src/server/main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
 	"fmt"
-	"time"
 	"io/ioutil"
-	"github.com/dlclark/regexp2"
-	"github.com/pkg/errors"
+	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/dlclark/regexp2"
+	"github.com/pkg/errors"
 )
 
 func main() {
-	fmt.Println(":8080")
-	http.HandleFunc("/contributions",contributionAPIHandle)
-	http.HandleFunc("/trending",trendingAPIHandle)
+	fmt.Println(":8090")
+	http.HandleFunc("/contributions", contributionAPIHandle)
+	http.HandleFunc("/trending", trendingAPIHandle)
 
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8090", nil)
 }
 
-func contributionAPIHandle(writer http.ResponseWriter,request *http.Request) {
+func contributionAPIHandle(writer http.ResponseWriter, request *http.Request) {
 	err := request.ParseForm()
 	if err != nil {
 		return
 	}
 
 	user := request.Form.Get("user")
-	contributions,_ := getContributions(user)
-	result,_ := json.Marshal(contributions)
+	contributions, _ := getContributions(user)
+	result, _ := json.Marshal(contributions)
 
-	writer.Header().Set("Content-Type","application/json; charset=utf-8")
-	fmt.Fprint(writer,string(result))
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprint(writer, string(result))
 }
 
-func trendingAPIHandle(writer http.ResponseWriter,request *http.Request) {
-	trending,_ := getTrending()
-	result,_ := json.Marshal(trending)
+func trendingAPIHandle(writer http.ResponseWriter, request *http.Request) {
+	trending, _ := getTrending()
+	result, _ := json.Marshal(trending)
 
-	writer.Header().Set("Content-Type","application/json; charset=utf-8")
-	fmt.Fprint(writer,string(result))
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprint(writer, string(result))
 }
 
 type Trending struct {
@@ -48,43 +49,43 @@ type Trending struct {
 }
 
 type Repository struct {
-	Name		string		`json:"name"`
-	Description	string		`json:"description"`
-	Url			string		`json:"url"`
-	Star		string		`json:"star"`
-	Fork		string		`json:"fork"`
-	Lang		string		`json:"lang"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Url         string `json:"url"`
+	Star        string `json:"star"`
+	Fork        string `json:"fork"`
+	Lang        string `json:"lang"`
 }
 
-func getTrending() (Trending,error){
+func getTrending() (Trending, error) {
 	requestUrl := "https://github.com/trending"
-	res,err := http.Get(requestUrl)
+	res, err := http.Get(requestUrl)
 	if err != nil {
-		return Trending{},err
+		return Trending{}, err
 	}
 
-	contentBytes,err := ioutil.ReadAll(res.Body)
+	contentBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil || contentBytes == nil {
-		return Trending{},err
+		return Trending{}, err
 	}
 
 	htmlContent := string(contentBytes)
 	repositories := resolveRepositories(htmlContent)
-	return Trending{Repositories:repositories},nil
+	return Trending{Repositories: repositories}, nil
 }
 
 func resolveRepositories(content string) []Repository {
 	repositoryItemExp := `<li class="col-12 d-block width-full py-4 border-bottom"[\s\S]*?>[\s\S]*?<\/li>`
 
 	regexp := regexp.MustCompile(repositoryItemExp)
-	match := regexp.FindAll([]byte(content),-1)
+	match := regexp.FindAll([]byte(content), -1)
 	if match == nil {
 		return nil
 	}
 
-	result := make([]Repository,0)
-	for i := 0; i< len(match);i++  {
-		result = append(result,resolveRepositoryTag(string(match[i])))
+	result := make([]Repository, 0)
+	for i := 0; i < len(match); i++ {
+		result = append(result, resolveRepositoryTag(string(match[i])))
 	}
 
 	return result
@@ -98,58 +99,57 @@ func resolveRepositoryTag(content string) Repository {
 	fork := stringFormat(getRepositoryFork(content))
 	url := "https://github.com" + name
 
-	return Repository{Name:name,Description:desc,Lang:lang,Star:star,Fork:fork,Url:url}
+	return Repository{Name: name, Description: desc, Lang: lang, Star: star, Fork: fork, Url: url}
 }
 
 func stringFormat(content string) string {
-	content = strings.Replace(content,"\n","",-1)
+	content = strings.Replace(content, "\n", "", -1)
 	content = strings.TrimSpace(content)
 	return content
 }
 
 func getRepositoryName(content string) string {
 	repositoryItemNameExp := `(?<=<h3>[\s\S]+<a href=").*(?=">)`
-	return findFirstOrDefaultMatch(content,repositoryItemNameExp)
+	return findFirstOrDefaultMatch(content, repositoryItemNameExp)
 }
 
 func getRepositoryLang(content string) string {
 	repositoryItemLangExp := `(?<=<span itemprop="programmingLanguage">)[\s\S]+?(?=<\/span>)`
-	return findFirstOrDefaultMatch(content,repositoryItemLangExp)
+	return findFirstOrDefaultMatch(content, repositoryItemLangExp)
 }
 
 func getRepositoryDescription(content string) string {
 	repositoryItemDescriptionExp := `(?<=<p class="col-9 d-inline-block text-gray m-0 pr-4">)[\s\S]+?(?=<\/p>)`
-	return findFirstOrDefaultMatch(content,repositoryItemDescriptionExp)
+	return findFirstOrDefaultMatch(content, repositoryItemDescriptionExp)
 }
 
 func getRepositoryStar(content string) string {
-	repositoryItemStarTagExp :=`<a class="muted-link d-inline-block mr-3" href="[\s\S]*?\/stargazers">[\s\S]*?<\/a>`
-	starTagContent := findFirstOrDefaultMatch(content,repositoryItemStarTagExp)
-	if starTagContent == ""{
+	repositoryItemStarTagExp := `<a class="muted-link d-inline-block mr-3" href="[\s\S]*?\/stargazers">[\s\S]*?<\/a>`
+	starTagContent := findFirstOrDefaultMatch(content, repositoryItemStarTagExp)
+	if starTagContent == "" {
 		return starTagContent
 	}
 
-	repositoryItemStarValueExp :=`<\/svg>([\s\S]*)<\/a>`
-	starValue := findFirstOrDefaultMatch(starTagContent,repositoryItemStarValueExp)
+	repositoryItemStarValueExp := `<\/svg>([\s\S]*)<\/a>`
+	starValue := findFirstOrDefaultMatch(starTagContent, repositoryItemStarValueExp)
 	return starValue
 }
 
 func getRepositoryFork(content string) string {
-	repositoryItemForkTagExp :=`<a class="muted-link d-inline-block mr-3" href=".*\/network">[\s\S]*?<\/a>`
-	forkTagContent := findFirstOrDefaultMatch(content,repositoryItemForkTagExp)
-	if forkTagContent == ""{
+	repositoryItemForkTagExp := `<a class="muted-link d-inline-block mr-3" href=".*\/network">[\s\S]*?<\/a>`
+	forkTagContent := findFirstOrDefaultMatch(content, repositoryItemForkTagExp)
+	if forkTagContent == "" {
 		return forkTagContent
 	}
 
-	repositoryItemForkValueExp :=`<\/svg>([\s\S]*)<\/a>`
-	forkValue := findFirstOrDefaultMatch(forkTagContent,repositoryItemForkValueExp)
+	repositoryItemForkValueExp := `<\/svg>([\s\S]*)<\/a>`
+	forkValue := findFirstOrDefaultMatch(forkTagContent, repositoryItemForkValueExp)
 	return forkValue
 }
 
-
-func findFirstOrDefaultMatch(content string,exp string) string {
-	regexp2 := regexp2.MustCompile(exp,0)
-	match,err := regexp2.FindStringMatch(content)
+func findFirstOrDefaultMatch(content string, exp string) string {
+	regexp2 := regexp2.MustCompile(exp, 0)
+	match, err := regexp2.FindStringMatch(content)
 	if err != nil || match == nil {
 		return ""
 	}
@@ -163,30 +163,30 @@ func findFirstOrDefaultMatch(content string,exp string) string {
 }
 
 type Contribution struct {
-	DataCount int		`json:"count"`
-	Date      string	`json:"date"`
-	Color	  string	`json:"color"`
+	DataCount int    `json:"count"`
+	Date      string `json:"date"`
+	Color     string `json:"color"`
 }
 
 func getContributions(userName string) ([]Contribution, error) {
 	if userName == "" {
-		return nil,errors.New("required:username")
+		return nil, errors.New("required:username")
 	}
 
 	currentTime := time.Now()
 	requestUrl := "https://github.com/users/" + userName + "/contributions?to=" + currentTime.Format("2006-01-02")
-	res,err := http.Get(requestUrl)
+	res, err := http.Get(requestUrl)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	contentBytes,err := ioutil.ReadAll(res.Body)
+	contentBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil || contentBytes == nil {
-		return nil,err
+		return nil, err
 	}
 
 	contentString := string(contentBytes)
-	return resolveContributions(contentString),nil
+	return resolveContributions(contentString), nil
 }
 
 func resolveContributions(content string) []Contribution {
@@ -195,26 +195,26 @@ func resolveContributions(content string) []Contribution {
 		return nil
 	}
 
-	contributions := make([]Contribution,0)
-	for i:=0;i<len(rectTags);i++{
+	contributions := make([]Contribution, 0)
+	for i := 0; i < len(rectTags); i++ {
 		contribution := createContributionByRectTag(rectTags[i])
-		contributions = append(contributions,contribution)
+		contributions = append(contributions, contribution)
 	}
 
 	return contributions
 }
 
-func resolveRectTags(content string)[]string{
+func resolveRectTags(content string) []string {
 	exp := `<rect.*?\/>`
 	regexp := regexp.MustCompile(exp)
-	match := regexp.FindAll([]byte(content),-1)
+	match := regexp.FindAll([]byte(content), -1)
 	if match == nil {
 		return nil
 	}
 
-	result := make([]string,0)
-	for i := 0; i< len(match);i++  {
-		result = append(result,string(match[i]))
+	result := make([]string, 0)
+	for i := 0; i < len(match); i++ {
+		result = append(result, string(match[i]))
 	}
 
 	return result
@@ -222,25 +222,25 @@ func resolveRectTags(content string)[]string{
 
 func createContributionByRectTag(tag string) Contribution {
 	exp := `(?<=<rect.*data-count=").*(?="\s*data-date.*\/>)|(?<=<rect.*\s*data-date=").*(?="\s?.*\/>)|(?<=<rect.*fill=").*(?="\s*data-count.*\/>)`
-	regexp2 := regexp2.MustCompile(exp,0)
-	colorMatchResult,err := regexp2.FindStringMatch(tag)
+	regexp2 := regexp2.MustCompile(exp, 0)
+	colorMatchResult, err := regexp2.FindStringMatch(tag)
 	if err != nil || colorMatchResult == nil {
 		return Contribution{}
 	}
 
-	dataCountMatch,_ := regexp2.FindNextMatch(colorMatchResult)
+	dataCountMatch, _ := regexp2.FindNextMatch(colorMatchResult)
 	if dataCountMatch == nil {
 		return Contribution{}
 	}
 
-	dateMatchResult,_ := regexp2.FindNextMatch(dataCountMatch)
+	dateMatchResult, _ := regexp2.FindNextMatch(dataCountMatch)
 	if dateMatchResult == nil {
 		return Contribution{}
 	}
 
-	dataCount,_ := strconv.Atoi(dataCountMatch.String())
+	dataCount, _ := strconv.Atoi(dataCountMatch.String())
 	date := dateMatchResult.String()
 	color := colorMatchResult.String()
 
-	return Contribution{Color:color,DataCount:dataCount,Date:date}
+	return Contribution{Color: color, DataCount: dataCount, Date: date}
 }
